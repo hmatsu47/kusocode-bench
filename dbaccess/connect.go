561d9ef6a1d119042e002f8edb33dc7b2f25ac7e
hmatsu47 <35142774+hmatsu47@users.noreply.github.com>
2022-01-24 09:30:47 +0900

    DB 接続先を localhost に

--- dbaccess/connect.go
+++ dbaccess/connect.go
@@ -11,7 +11,7 @@ func Connect() (db *sql.DB) {
 	dbDriver := "mysql"
 	dbUser := "root"
 	dbPass := "root@kus0c0de"
-	dbServer := "192.168.71.36:3306"
+	dbServer := "127.0.0.1:3306"
 	dbName := "picture"
 	dbOption := "charset=utf8"
 	dataSourceName := fmt.Sprintf("%s:%s@tcp(%s)/%s?%s", dbUser, dbPass, dbServer, dbName, dbOption)
